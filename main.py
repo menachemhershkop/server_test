340245982f69ef77fd10d0e4b7fa4705aec8d366
mendy hershkop <hers.mm@gmail.com>
2025-11-13 12:12:30 +0200

    building the caesar methods in server

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
 from pydantic import BaseModel
+
+from encrypt.caeser import Caesar
 from encrypt.fance import Fence
 from list_names.saving_names import saveing_names
 
-class Item(BaseModel):
+class Massage(BaseModel):
     text: str
     offset: int
     mode: str
@@ -22,13 +24,17 @@ def save_user(name):
     saveing_names(name)
     return {"msg":"saved user"}
 @app.post("/caesar")
-def caeser(msg):
-    pass
+def caesar(msg:Massage):
+    caesar=Caesar()
+    if msg.mode=="encrypt":
+        return {"encrypted_text":caesar.encrypy(msg.text,msg.offset)}
+    if msg.mode=="decrypt":
+        return {"decryptes_text":caesar.encrypy(msg.text,msg.offset)}
 @app.get("/fence/encrypt")
 def encrypt(text):
     encrypt=Fence()
     return {"encrypted_text":encrypt.encrypy(text)}
 @app.post("/fence/decrypt")
-def decrypt(msg):
+def decrypt(msg:Massage):
     decrypt=Fence()
-    return {"decrypted":decrypt.encrypy(msg)}
+    return {"decrypted":decrypt.encrypy(msg.text)}
