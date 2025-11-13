d3dcb8c2f438bdf6e0e6f39b2fbcd5c69477e802
mendy hershkop <hers.mm@gmail.com>
2025-11-13 12:39:23 +0200

    create decrypt in fence

--- encrypt/fance.py
+++ encrypt/fance.py
@@ -10,3 +10,13 @@ class Fence:
             else:
                 odd.append(msg[i])
         return "".join(double+odd)
+    def decrypt(self,msg):
+        first=msg[:len(msg)//2+len(msg)%2]
+        seccend=msg[len(msg)//2+len(msg)%2:]
+        new=[]
+        for i in range(len(msg)):
+            if i < len(first):
+                new.append(first[i])
+            if i < len(seccend):
+                new.append(seccend[i])
+        return "".join(new)
