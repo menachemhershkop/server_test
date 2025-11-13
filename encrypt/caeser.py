384441f59ad9289992f2967ad6e3249c785d4c44
mendy hershkop <hers.mm@gmail.com>
2025-11-13 12:09:46 +0200

    create decrypt/encrypt function

--- encrypt/caeser.py
+++ encrypt/caeser.py
@@ -1,4 +1,4 @@
-class Caeser:
+class Caesar:
     def encrypy(self,msg,offset):
         msg=list(msg.lower())
         abc= list(map(chr, range(97, 123)))
@@ -6,11 +6,20 @@ class Caeser:
             for x,z in enumerate(abc):
                 if y ==z:
                     if x+offset>len(abc):
-                        x-=len(abc)
-                        msg[i]=abc[x+offset]
+                        x=(x+offset)%25
+                        msg[i]=abc[x]
                     else:
                         msg[i] = abc[x + offset]
-        print(msg)
-a=Caeser()
-a.encrypy("abc",49)
-
+        return "".join(msg)
+    def decrypt(self,msg,offset):
+        msg = list(msg.lower())
+        abc = list(map(chr, range(97, 123)))
+        for i,y in enumerate(msg):
+            for x,z in enumerate(abc):
+                if y ==z:
+                    if x-offset<0:
+                        x=(x+offset)%25
+                        msg[i]=abc[x]
+                    else:
+                        msg[i] = abc[x - offset]
+        return "".join(msg)
